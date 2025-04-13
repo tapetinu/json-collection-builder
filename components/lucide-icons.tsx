2401d5015e6f9649fad7ca98ee923ecf6b979df2
tapetinu <151819685+tapetinu@users.noreply.github.com>
2025-04-13 19:35:24 +0800

    Add lucide icons - ArrowLeft - ArrowRight

---
+++ components/lucide-icons.tsx
@@ -0,0 +1,41 @@
+import { SVGAttributes } from "react";
+
+export function ArrowLeft({ ...props }: SVGAttributes<SVGSVGElement>) {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width="24"
+			height="24"
+			viewBox="0 0 24 24"
+			fill="none"
+			stroke="currentColor"
+			strokeWidth="2"
+			strokeLinecap="round"
+			strokeLinejoin="round"
+			className="lucide lucide-arrow-left-icon lucide-arrow-left"
+			{...props}>
+			<path d="m12 19-7-7 7-7" />
+			<path d="M19 12H5" />
+		</svg>
+	);
+}
+
+export function ArrowRight({ ...props }: SVGAttributes<SVGSVGElement>) {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width="24"
+			height="24"
+			viewBox="0 0 24 24"
+			fill="none"
+			stroke="currentColor"
+			strokeWidth="2"
+			strokeLinecap="round"
+			strokeLinejoin="round"
+			className="lucide lucide-arrow-right-icon lucide-arrow-right"
+			{...props}>
+			<path d="M5 12h14" />
+			<path d="m12 5 7 7-7 7" />
+		</svg>
+	);
+}
